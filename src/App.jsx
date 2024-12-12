092664329bfbc5982807f7b6f873f600e0ac5ed1
NiteshkTiwari <nt08802@gmail.com>
2024-12-12 20:59:55 +0530

    Praticing !!!

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <div className="accordion">
+        {faqs.map((el, i) => (
+          <Card title={el.title} text={el.text} num={i} key={el.title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Card({ title, text, num }) {
+  const [active, setActive] = useState(false);
+  return (
+    <div
+      className={`item ${active ? "open" : ""}`}
+      onClick={() => setActive((c) => !c)}
+    >
+      <p className="number">{num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{active ? "-" : "+"}</p>
+      {active ? <div className="content-box">{text}</div> : null}
+    </div>
+  );
+}
+
+export default App;
